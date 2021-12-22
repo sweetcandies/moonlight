71b587ef66817731a8a5d1c182c863930ef67052
Funny <hanyuefan776@163.com>
2021-12-22 16:04:51 +0800

    测试用例

--- gateway/src/test/java/com/funiverise/authority/GatewayApplicationTest.java
+++ gateway/src/test/java/com/funiverise/authority/GatewayApplicationTest.java
@@ -1,12 +1,19 @@
 package com.funiverise.authority;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
+@EnableScheduling
 public class GatewayApplicationTest 
 {
     /**
@@ -16,5 +23,18 @@ public class GatewayApplicationTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void atomicNumFunctionTest() {
+        AtomicLong num = new AtomicLong(10);
+        num.addAndGet(-5);
+        System.out.println(num);
+    }
+
+    @Test
+    @Scheduled
+    public void testTimeFormatter() {
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
 }
